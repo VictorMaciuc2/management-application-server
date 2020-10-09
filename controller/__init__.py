fa599ad110d0aaa95543f11a1f702faef346e8b2
Mihalut Filip <mihalutafilip@yahoo.com>
2020-10-09 16:44:16 +0300

    Add initial application

---
+++ controller/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/people')
+def getNames():
+    return {
+        'people': ['Filip']
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
