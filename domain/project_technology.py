09b267fa9c1ddbcf4b64c89ebb11c9c5b3a747f0
NewBestX <nbgamingx@gmail.com>
2020-11-29 23:16:58 +0200

    Changed project_technology constructor to match class name

--- domain/project_technology.py
+++ domain/project_technology.py
@@ -6,9 +6,9 @@ class Project_Technology(db.Model):
     project_id = db.Column(db.Integer, db.ForeignKey('Projects.id'), primary_key=True)
     technology_id = db.Column(db.Integer, db.ForeignKey('Technologies.id'), primary_key=True)
 
-    def __init__(self, user_id, project_id):
-        self.technology_id = user_id
+    def __init__(self, project_id, technology_id):
         self.project_id = project_id
+        self.technology_id = technology_id
 
     def get_technology_id(self):
         return self.technology_id
